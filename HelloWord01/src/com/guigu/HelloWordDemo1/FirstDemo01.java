0c00ce698b236b9df1a280aa952bd1d75b2df4e4
JieTang <2398758854@qq.com>
2024-04-26 10:57:34 +0800

    Initial commit

---
+++ HelloWord01/src/com/guigu/HelloWordDemo1/FirstDemo01.java
@@ -0,0 +1,14 @@
+package com.guigu.HelloWordDemo1;
+
+
+/**
+ * 功能描述
+ *
+ * @author TJ
+ * @date 2024/04/26  10:46 AM
+ */
+public class FirstDemo01 {
+	public static void main(String[] args) {
+		System.out.println("你好！世界。");
+	}
+}
